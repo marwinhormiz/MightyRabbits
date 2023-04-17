b0d189d4982bdd5b81cd1de13cd6b1edf6f4b945
marwinhormiz <marwinhormiz@gmail.com>
2023-04-17 13:17:22 +0200

    fix

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from firebase_manager import FirebaseManager
 
-CREDENTIALS = "solar-team-development-firebase-adminsdk-ryi02-02010eb561.json"
-REFERENCE = "https://solar-team-development-default-rtdb.europe-west1.firebasedatabase.app"
-DATABASE_NAME = "data"
+CREDENTIALS = ""
+REFERENCE = ""
+DATABASE_NAME = ""
 
 def main():
     manager = FirebaseManager(cred_path=CREDENTIALS, db_url=REFERENCE, db_name=DATABASE_NAME)
