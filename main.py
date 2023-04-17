afa959efc657f81853e7016f8edac9c2843e3000
marwinhormiz <marwinhormiz@gmail.com>
2023-04-17 20:16:13 +0200

    fix

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ DATABASE_NAME = ""
 def main():
     manager = FirebaseManager(cred_path=CREDENTIALS, db_url=REFERENCE, db_name=DATABASE_NAME)
     # Skriv din kod här
-    print(manager.get())
+    # Använd manager.push(data) för att lägga in ny data i din databas
+    # Använd manager.get()
+    data = 5
+    manager.push(data)
+
+    get_data = manager.get()
+    print(get_data)
 
 if __name__ == '__main__':
     main()
